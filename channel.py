dd0f7501f9886ad3b3618a16dc69388a92a864bf
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-06-18 12:30:03 -0400

    Update channel.py

--- channel.py
+++ channel.py
@@ -16,53 +16,12 @@ from sionna.fec.ldpc.decoding import LDPC5GDecoder
 
 
 
-class E2EModel(tf.keras.Model):
-    """End-to-end model for (GNN-)decoder evaluation.
 
-    Parameters
-    ----------
-    encoder: Layer or None
-        Encoder layer, no encoding applied if None.
 
-    decoder: Layer or None
-        Decoder layer, no decoding applied if None.
 
-    k: int
-        Number of information bits per codeword.
-
-    n: int
-        Codeword lengths.
-
-    return_infobits: Boolean
-        Defaults to False. If True, only the ``k`` information bits are
-        returned. Must be supported be the decoder as well.
-
-    es_no: Boolean
-        Defaults to False. If True, the SNR is not rate-adjusted (i.e., Es/N0).
-
-    Input
-    -----
-        batch_size: int or tf.int
-            The batch_size used for the simulation.
-
-        ebno_db: float or tf.float
-            A float defining the simulation SNR.
-
-    Output
-    ------
-        (c, llr):
-            Tuple:
-
-        c: tf.float32
-            A tensor of shape `[batch_size, n] of 0s and 1s containing the
-            transmitted codeword bits.
-
-        llr: tf.float32
-            A tensor of shape `[batch_size, n] of llrs containing estimated on
-            the codeword bits.
-    """
-
-    def __init__(self, encoder, decoder, model, 
+class E2EModelDDECC(tf.keras.Model):
+    def __init__(self, encoder, decoder,  
+                       batch_size=1, 
                        return_infobits=False,
                        es_no=False, 
                        decoder_active=False):        
@@ -72,50 +31,84 @@ class E2EModel(tf.keras.Model):
         self._k = encoder._k
 
         self._binary_source = BinarySource()
-        self._num_bits_per_symbol = 2
-        self._mapper = Mapper("qam", self._num_bits_per_symbol) #
-        self._demapper = Demapper("app", "qam", self._num_bits_per_symbol) #
-        self._channel = AWGN() #
-        self._decoder = decoder
+        self._num_bits_per_symbol = 4 # QAM16
+        
+        # Channel
+        ############################
+        self.build_enc = True
         self._encoder = encoder
-        # self._return_infobits = return_infobits
-        # self._es_no = es_no
+        self._mapper = Mapper("qam", self._num_bits_per_symbol) #
 
-    @tf.function(jit_compile=True)
-    def call(self, batch_size, ebno_db):
+        self._channel = AWGN() #
+        
+        self._demapper = Demapper("app", "qam", self._num_bits_per_symbol) #
+        self._decoder = decoder # DDECCT
+        ############################
 
-        # no rate-adjustment for uncoded transmission or es_no scenario
-        if self._decoder is not None and self._es_no==False:
-            no = ebnodb2no(ebno_db, self._num_bits_per_symbol, self._k/self._n)
+        self._return_infobits = return_infobits
+        self._es_no = es_no
+        
+        self._batch_size = batch_size
+
+    def train(self):
+      pass
+    
+    def test(self):
+      pass
+
+    # @tf.function(jit_compile=True)
+    def call(self, ebno_db):
+        # Noise Variance
+        if self._decoder is not None and self._es_no==False: # no rate-adjustment for uncoded transmission or es_no scenario
+            no = ebnodb2no(ebno_db, self._num_bits_per_symbol, self._k/self._n) ### LOOK UP EBNODB2NO
         else: #for uncoded transmissions the rate is 1
-            no = ebnodb2no(ebno_db, self._num_bits_per_symbol, 1)
+            no = ebnodb2no(ebno_db, self._num_bits_per_symbol, 1) ###
+        no = tf.expand_dims(tf.cast(no, tf.float32), axis=-1) # turn to float32, turns shape (9,) -> (9,1)
+        print("no, ebno_db: ", no.shape, ebno_db.shape)
 
-        b = self._binary_source([batch_size, self._k])
+        b = self._binary_source([self._batch_size, self._encoder._k]) # (batch_size, k), k information bits
+        print("bit: ", b.shape) # print(b.shape[-1]==self._k, b.shape, self._k, self._n - self._k)
+
+        # Encode info bits to codeword
         if self._encoder is not None:
-            c = self._encoder(b)
+            # Build encoder
+            if self.build_enc:
+              self._encoder.build(b.shape) 
+              self.build_enc = False
+
+            c = self._encoder(b) ##### c = G @ b.T, (n_ldpc,k) @ (k,1)
         else:
             c = b
 
+        print("n, c: ", self._n, c.shape)
         # check that rate calculations are correct
-        assert self._n==c.shape[-1], "Invalid value of n."
+        assert self._n == c.shape[-1], "Invalid value of n."
 
         # zero padding to support odd codeword lengths
-        if self._n%2==1:
-            c_pad = tf.concat([c, tf.zeros([batch_size, 1])], axis=1)
+        if self._n%2 == 1:
+            c_pad = tf.concat([c, tf.zeros([self._batch_size, 1])], axis=1)
         else: # no padding
             c_pad = c
-        x = self._mapper(c_pad)
+        print("c_pad, c: ", c_pad.shape, c.shape)
 
-        y = self._channel([x, no])
+        # Channel
+        ############################
+        x = self._mapper(c_pad)
+        y = self._channel([x, no]) #
         llr = self._demapper([y, no])
+        ############################
+        print("y, no: ", y.shape, no.shape)
 
         # remove zero padded bit at the end
-        if self._n%2==1:
+        if self._n%2 == 1:
             llr = llr[:,:-1]
+        llr = torch.tensor(llr.numpy())
+        print("llr: ", llr.shape,)# b, c, x, y)
 
-        # and run the decoder
+        # run the decoder
         if self._decoder is not None:
-            llr = self._decoder(llr)
+            llr = self._decoder(llr, time_step=0) # 9 no values, 100 bits of data, time step 0
+        print("%%%%%%%%%%%%%%%%%%%")
 
         if self._return_infobits:
             return b, llr
