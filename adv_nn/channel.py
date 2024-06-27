8caba2a05f1b3bf2e1169c94415a701d5cc9d972
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-06-26 23:01:48 -0400

    Update channel.py

--- adv_nn/channel.py
+++ adv_nn/channel.py
@@ -1,3 +1,43 @@
+# pytorch or tensorflow?
+
+# Channel should take in a generator G() (n,n), discriminator D() (n,n), codeword c (n,1), noise z (n,1), pcm H (m,n), 
+    # such that:
+      # c_hat, z_hat = D(G(c, z, H)), 
+                                  # where z is optional or 0,
+                                  # D can be trainable or have static weights,
+                                  # and D recieves H from G.
+
+# TRAIN
+  # for {batch_ix, H, (m, c, z, r, c_llr, r_llr, magnitude, syndrome)} in enumerate(test_data):
+      # z is either 0 or a structured noise process
+      # z_G ~ G(c, z, H) such that,
+      # z_out = z + z_G
+      #
+      # r = c + z_out
+      # c_hat, z_hat = D(r, H) 
+      # 
+      # loss = loss_fn(c_hat, c)
+      #
+      # 
+
+
+# TEST
+  # for {batch_ix, H, (m, c, z, r, c_llr, r_llr, magnitude, syndrome)} in enumerate(test_data):
+
+      # for eb_no in Eb/Nos:
+          # sigma calculated using eb_no
+          # z ~ N(0, sigma) normally dist.
+          # r = c + z
+          
+          # c_hat, z_hat = D(r, H) 
+          
+          # print(c_hat, c)
+          # print(BER(c_hat, c), FER(c_hat, c))     bit error rate, forward error rate
+        
+      # Eb = b/s bits per second transmitted through a channel 
+      # No = Hz, Hz = k*T, for every T temperature units and k is Boltzmann's constant meaning more energy 
+
+
 class Channel:
   def __init__(self):
     pass
