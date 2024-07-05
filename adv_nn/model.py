6a65ff3923766f19838c28d5b835759e7167e3e5
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-05 09:29:31 -0400

    Update model.py

--- adv_nn/model.py
+++ adv_nn/model.py
@@ -42,6 +42,10 @@ class Generator(Layer):
 # Construct discriminator (decoder using reverse diffusion)
     # Will have to come up with ways to try to decode the noised codeword against specific noise
     # that will be trying to fool it. 
+    
+    # For optimization:
+        # use Linformer having a O(n) on top of already improved complexity using the pcm mask
+        # use split diffusion to improve accuracy and efficiency by guiding model rather than EMA
 
 class Discriminator(Layer):
     def __init__(self, ls_active=True):
@@ -53,7 +57,7 @@ class Discriminator(Layer):
         
     def call(self, r_t):
         for i in range(self.pcm.shape[0]):
-            r_t, z_hat,  t = self.rev_diff_call(r_t)
+            r_t, z_hat, t = self.rev_diff_call(r_t)
             
             if (r_t @ self.pcm)==0:
                 return r_t, z_hat, i            
@@ -62,7 +66,7 @@ class Discriminator(Layer):
     # Refines recieved codeword r at time t
     def rev_diff_call(self, r_t): 
         t = ( self.pcm @ to_bin(r_t) ).sum() # ix for the 'time step' t in the diffusion # 'time step' t is really a error estimate of the syndrome
-        c_hat, z_hat = self.tran_call(r_t, t)
+        z_hat = self.tran_call(r_t, t)
         
         l = self.line_search() if self.ls_active else 1.
         factor = ( self.betas_bar[t]*self.beta[t] / (self.betas_bar[t] + self.beta[t]) ) # theoretical step size
@@ -89,7 +93,7 @@ class Discriminator(Layer):
         emb_t = time_emb * emb
         self.decoder(emb_t, self.mask, time_emb)
         
-        return c_hat, z_hat
+        return z_hat
 
 
 class E2EModel(tf.keras.Model):
