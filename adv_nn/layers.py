aac28585458abddd125924bf2fca2313b4121bc2
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-08 14:15:12 -0400

    Update layers.py

--- adv_nn/layers.py
+++ adv_nn/layers.py
@@ -4,17 +4,20 @@ from tensorflow.keras.initializers import GlorotUniform
 
 
 class MHAttention(tf.keras.layers.Layer):
-    def __init__(self, dims, heads):
+    def __init__(self, dims, heads, linear=True):
         super().__init__()
         assert (dims % heads) == 0, 'dimension must be divisible by the number of heads'
-
+        self.linear = linear
+            
     def call(self):
-        self.lin_attention(k, q, v)
+        out_att = self.lin_attention(k, q, v) if self.linear else self.attention(k, q, v)
         return 
 
     def n_to_k(self):
         pass
 
-    def lin_attention(self):
+    def lin_attention(self): # O(n)
         pass
-        
+
+    def attention(self): # O(n^2)
+        pass
