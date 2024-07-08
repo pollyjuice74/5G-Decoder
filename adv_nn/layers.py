158a19640cdc27f4bb52626b594f57a748c702fd
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-08 13:10:28 -0400

    Update layers.py

--- adv_nn/layers.py
+++ adv_nn/layers.py
@@ -1,1 +1,20 @@
 import tensorflow as tf
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.initializers import GlorotUniform
+
+
+class MHAttention(tf.keras.layers.Layer):
+    def __init__(self, dims, heads):
+        super().__init__()
+        assert (dims % heads) == 0, 'dimension must be divisible by the number of heads'
+
+    def call(self):
+        self.lin_attention(k, q, v)
+        return 
+
+    def n_to_k(self):
+        pass
+
+    def lin_attention(self):
+        pass
+        
