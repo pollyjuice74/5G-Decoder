8946b9f4c21aced75ca0829f6470b9397de22e78
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-08 15:27:14 -0400

    Update layers.py

--- adv_nn/layers.py
+++ adv_nn/layers.py
@@ -4,20 +4,30 @@ from tensorflow.keras.initializers import GlorotUniform
 
 
 class MHAttention(tf.keras.layers.Layer):
-    def __init__(self, dims, heads, linear=True):
+    def __init__(self, dims, heads, linear=True, k=256):
         super().__init__()
         assert (dims % heads) == 0, 'dimension must be divisible by the number of heads'
         self.linear = linear
+        self.dims = dims
+        self.heads = heads
+        self.dim_head = dims // heads
+        self.k = k
+        
+        self.to_q, self.to_k, self.to_v = [ Dense(self.dim_head * heads, use_bias=False) for _ in range(3) ]
+        self.to_out = Dense(dims)
+        self.dropout = Dropout(dims) # to d-dimentional embeddings
             
-    def call(self):
-        out_att = self.lin_attention(k, q, v) if self.linear else self.attention(k, q, v)
-        return 
+    def call(self, x, mask=None):
+        out_att = self.lin_attention(x, mask) if self.linear else self.attention(x, mask)
+        return out_attn
 
     def n_to_k(self):
         pass
 
-    def lin_attention(self): # O(n)
+    def lin_attention(self, x, mask): # O(n)
+        query, key, val = self.to_q(x), self.to_k(x), self.to_v(x)
         pass
 
-    def attention(self): # O(n^2)
+    def attention(self, mask): # O(n^2)
+        query, key, val = self.to_q(x), self.to_k(x), self.to_v(x)
         pass
