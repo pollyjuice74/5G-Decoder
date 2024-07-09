ca575f345966e842fac946b910c8c1ee29330990
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-08 22:32:36 -0400

    Update layers.py

--- adv_nn/layers.py
+++ adv_nn/layers.py
@@ -1,15 +1,23 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.initializers import GlorotUniform
+from tensorflow.keras.activations import gelu
 
 
 class FeedForward(tf.keras.layers.Layer):
-    def __init__(self):
-        
+    def __init__(self, dim, mult=4, activation=None, dropout=0.01):
+        self.w1 = Dense(dim * mult)
+        self.w2 = Dense(dim)
+        self.dropout = Dropout(dropout) 
+        self.act = activation if activation else gelu
+
+    def call(self, x):
+        x = self.act(self.w1(x))
+        return self.w2(self.dropout(x))
 
 
 class MHAttention(tf.keras.layers.Layer):
-    def __init__(self, dims, heads, linear=True, k=256):
+    def __init__(self, dims, heads, linear=True, k=256, dropout=0.01):
         super().__init__()
         assert (dims % heads) == 0, 'dimension must be divisible by the number of heads'
         self.linear = linear
@@ -24,7 +32,7 @@ class MHAttention(tf.keras.layers.Layer):
         
         self.to_q, self.to_k, self.to_v = [ Dense(self.dim_head * heads, use_bias=False) for _ in range(3) ]
         self.to_out = Dense(dims)
-        self.dropout = Dropout(dims) # to d-dimentional embeddings
+        self.dropout = Dropout(dropout) # to d-dimentional embeddings
             
     def call(self, x, mask=None):
         out_att = self.lin_attention(x, mask) if self.linear else self.attention(x, mask)
