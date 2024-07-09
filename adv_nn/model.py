a15a0b31a8e9f9f2cb212547f71462ab28e38f41
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-09 10:17:33 -0400

    Update model.py

--- adv_nn/model.py
+++ adv_nn/model.py
@@ -9,6 +9,7 @@ import numpy as np
 
 from decoder import * 
 
+
 ## Generative Adversarial Network approach
 
 # Should  use specifically 5G standard LDPC/Polar codes
@@ -17,57 +18,37 @@ from decoder import *
         # Polar: Successive Cancellation Listing, Successive Cancellation
 
     # Could store different weights for generator and discriminator, 
-    # make a super noise generator that will always attack really good at the discriminator 
+    # make a super noise generator that will always attack really good at the discriminator
+
 class DataSet():
     def __init__(self):
         pass
-    
 
-# Construct generator (encoder using forward diffusion to simulate channel)
-    # By simulating channel it will try to come up with ways to fool discriminator/decoder
-    # through noising the original codeword
-
-    # Could make a general generator for GNN, ECCT, DDECCT, and AECC(Adversarial Error Correcting Code) 
-    # to compare how different models learn or have weaknesses in decoding. 
-class Generator(Layer):
-    def __init__(self):
-        pass
-
-    def call(self, c):
-        emb = 
-        return z
-
-    def create_mask(self, H):
-        return H_mask
+class Args(Data):
+    def __init__(self, type, ls_active=True, beta_steps=10, t_layers=1, sigma):
         
+        assert type in ["gen", "dis"] "Type must be: 'gen', Generator or 'dis', Discriminator."
+        self.type = type
+        
+        self.ls_active = True
+        self.beta_steps
+        self.sigma
 
-# Construct discriminator (decoder using reverse diffusion)
-    # Will have to come up with ways to try to decode the noised codeword against specific noise
-    # that will be trying to fool it. 
+        self.d_model
+        self.t_layers = t_layers # transformer layers
     
-    # For optimization:
-        # use Linformer having a O(n) on top of already improved complexity using the pcm mask
-        # use split diffusion to improve accuracy and efficiency by guiding model rather than EMA
 
-class Discriminator(Layer):
-    def __init__(self, ls_active=True):
-        self.beta 
-        self.betas_bar
-        self.ls_active = ls_active
-
+class BaseDecoder():
+    def __init__(self, args):
+        self.betas = tf.linespace(1e-3, 1e-2, args.beta_steps)*0 + args.sigma 
+        self.betas_bar = tf.math.cumsum(self.betas, 0)
+        self.ls_active = args.ls_active
+    
         self.src_embed
         self.time_embed
         self.fc
-        
-    def call(self, r_t):
-        for i in range(self.pcm.shape[0]):
-            r_t, z_hat, t = self.rev_diff_call(r_t)
-            
-            if (r_t @ self.pcm)==0:
-                return r_t, z_hat, i            
-        return r_t, z_hat, i
 
-    # Refines recieved codeword r at time t
+     # Refines recieved codeword r at time t
     def rev_diff_call(self, r_t): 
         t = ( self.pcm @ to_bin(r_t) ).sum() # ix for the 'time step' t in the diffusion # 'time step' t is really a error estimate of the syndrome
         z_hat = self.tran_call(r_t, t)
@@ -101,12 +82,50 @@ class Discriminator(Layer):
         emb_t = self.decoder(emb_t, self.mask, time_emb)
         z_hat = self.fc(emb_t)
         return z_hat
+    
+    def create_mask(self, H):
+        return H_mask
 
 
-class E2EModel(tf.keras.Model):
+# Construct discriminator (decoder using reverse diffusion)
+    # Will have to come up with ways to try to decode the noised codeword against specific noise
+    # that will be trying to fool it. 
+    
+    # For optimization:
+        # use Linformer having a O(n) on top of already improved complexity using the pcm mask
+        # use split diffusion to improve accuracy and efficiency by guiding model rather than EMA
+
+class Discriminator(Layer):
+   def __init__(self):
+        pass
+       
+   def call(self, r_t):
+       for i in range(self.pcm.shape[0]):
+           r_t, z_hat, t = self.rev_diff_call(r_t)
+        
+           if (r_t @ self.pcm)==0:
+               return r_t, z_hat, t, i            
+       return r_t, z_hat, t, i
+
+
+# Construct generator (encoder using forward diffusion to simulate channel)
+    # By simulating channel it will try to come up with ways to fool discriminator/decoder
+    # through noising the original codeword
+
+    # Could make a general generator for GNN, ECCT, DDECCT, and AECC(Adversarial Error Correcting Code) 
+    # to compare how different models learn or have weaknesses in decoding. 
+
+class Generator(Layer):
     def __init__(self):
         pass
 
+    def call(self, c):
+        emb = 
+        return z
+
+        
+
+   
 
 
 
