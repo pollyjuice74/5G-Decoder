0a7684940897e3b2d09b627f48b8888006226ed9
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-08 21:55:03 -0400

    Update decoder.py

--- adv_nn/decoder.py
+++ adv_nn/decoder.py
@@ -1,2 +1,9 @@
 class Decoder():
-  
+    def init(self, N):
+      self.N = N
+      self.transformer = # mask
+
+    def call(self, x):
+        for _ in range(self.N):
+            x = self.transformer(x)
+        return x
