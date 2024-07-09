f4c5b6bc4f17d4d8e879408d0a53e87b9e9ea521
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-09 16:55:06 -0400

    Update model.py

--- adv_nn/model.py
+++ adv_nn/model.py
@@ -4,6 +4,8 @@ import numpy as np
 
 from decoder import * 
 
+from DDECC.src.codes import Get_Generator_and_Parity
+
 ## Generative Adversarial Network approach
 
 # Should  use specifically 5G standard LDPC/Polar codes
@@ -20,24 +22,38 @@ class DataSet():
 
 
 class Args():
-    def __init__(self, type, sigma, ls_active=True, beta_steps=10, t_layers=1,):
+    def __init__(self, model_type, code_type='LDPC', n=121, k=80, sigma=0.1, ls_active=True, beta_steps=10, t_layers=1,):
+        assert model_type in ['gen', 'dis'], "Type must be: 'gen', Generator or 'dis', Discriminator."
+        assert code_type in ['POLAR', 'BCH', 'CCSDS', 'LDPC', 'MACKAY'], "Invalid linear code type."
         
-        assert type in ["gen", "dis"], "Type must be: 'gen', Generator or 'dis', Discriminator."
-        self.type = type
-
-        self.code = code
-        
+        self.model_type = model_type
+        self.code_type = code_type
+        self.code = self.get_code(n,k)
         self.ls_active = True
-        self.beta_steps = beta_steps
         self.sigma = sigma
+      
+        self.beta_steps = beta_steps
+        self.N_steps = code.pc_matrix.shape[0]+5  ###################### Number of diffusion steps
 
         self.d_model = d_model
         self.t_layers = t_layers # transformer layers
 
         self.mask = create_mask(H)
     
-    def create_mask(self, H):
+    def create_mask(self):
+        H = self.code.H
         return H_mask
+
+    def get_code(self, n, k):
+        class Code():
+            pass
+        code = Code()
+        code.n, code.k = n, k
+        code.code_type = self.code_type
+    
+        G, H = Get_Generator_and_Parity(code)
+        code.G, code.H = tf.convert_to_tensor(G), tf.convert_to_tensor(H)
+        return code
     
 
 # Base model for the Generator and Discriminator models, 
