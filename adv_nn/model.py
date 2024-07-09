d3eb512e07d379812759564acd9ea6e55f666b35
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-09 11:35:29 -0400

    Update model.py

--- adv_nn/model.py
+++ adv_nn/model.py
@@ -4,7 +4,7 @@ from torch_geometric.nn import GPSConv
 from torch_geometric.data import Data
 
 import tensorflow as tf
-from tensorflow.keras.layers import Layer
+from tensorflow.keras.layers import Layer, Embedding, Dense
 import numpy as np
 
 from decoder import * 
@@ -24,11 +24,14 @@ class DataSet():
     def __init__(self):
         pass
 
+
 class Args(Data):
     def __init__(self, type, ls_active=True, beta_steps=10, t_layers=1, sigma):
         
         assert type in ["gen", "dis"] "Type must be: 'gen', Generator or 'dis', Discriminator."
         self.type = type
+
+        self.code = code
         
         self.ls_active = True
         self.beta_steps
@@ -36,30 +39,19 @@ class Args(Data):
 
         self.d_model
         self.t_layers = t_layers # transformer layers
+
+        self.mask = create_mask(H)
     
+    def create_mask(self, H):
+        return H_mask
+    
+
+# Base model for the Generator and Discriminator models, 
+    # contains diffusion computations, line search and creates mask of pcm.
 
 class TransformerDiffusion():
     def __init__(self, args):
-        self.betas = tf.linespace(1e-3, 1e-2, args.beta_steps)*0 + args.sigma 
-        self.betas_bar = tf.math.cumsum(self.betas, 0)
-        self.ls_active = args.ls_active
-    
-        self.src_embed
-        self.time_embed
-        self.fc
-
-     # Refines recieved codeword r at time t
-    def rev_diff_call(self, r_t): 
-        t = ( self.pcm @ to_bin(r_t) ).sum() # ix for the 'time step' t in the diffusion # 'time step' t is really a error estimate of the syndrome
-        z_hat = self.tran_call(r_t, t)
-        
-        factor = ( self.betas_bar[t]*self.beta[t] / (self.betas_bar[t] + self.beta[t]) ) # theoretical step size
-        err_hat = r_t - tf.sign(z_hat*rt)
-        l = self.line_search() if self.ls_active else 1.
-
-        r_t1 = r_t - (l * factor * err_hat) # refined estimate of the codeword for the diffusion step
-        # r_t1[t==0] = r_t[t==0] # if cw has 0 synd. keep as is
-        return r_t1, z_hat, t # r at time t-1
+        pass
 
     # optimal lambda l for theoretical and for error prediction
     def line_search(self, factor, err_hat):
@@ -67,24 +59,6 @@ class TransformerDiffusion():
         syndromes = to_bin(r_t - l_values*(factor*err_hat)) @ self.pcm
         ix = syndromes.argmin() # get the index of the min syndrome
         return l_values[ix]
-        
-    # Extracts noise estimate z_hat from r
-    def tran_call(self, r_t, t):
-        syndrome = self.pcm @ to_bin(r_t) #(m,1) check nodes
-        magnitude = tf.abs(r_t) #(n,1) variable nodes
-
-        nodes = tf.concat([magnitude, syndrome]) # data for vertices, shape (n+m,1)
-        
-        emb = self.src_emb * nodes
-        time_emb = self.time_embed(t) 
-        
-        emb_t = time_emb * emb
-        emb_t = self.decoder(emb_t, self.mask, time_emb)
-        z_hat = self.fc(emb_t)
-        return z_hat
-    
-    def create_mask(self, H):
-        return H_mask
 
 
 # Construct discriminator (decoder using reverse diffusion)
@@ -95,17 +69,54 @@ class TransformerDiffusion():
         # use Linformer having a O(n) on top of already improved complexity using the pcm mask
         # use split diffusion to improve accuracy and efficiency by guiding model rather than EMA
 
-class Discriminator(Layer):
-   def __init__(self):
-        pass
+class Discriminator(TransformerDiffusion):
+    def __init__(self, args):
+        self.betas = tf.linespace(1e-3, 1e-2, args.beta_steps)*0 + args.sigma 
+        self.betas_bar = tf.math.cumsum(self.betas, 0)
+        self.ls_active = args.ls_active
+        code = args.code
+        
+        self.src_embed = tf.Variable( tf.random.uniform(code.n + code.m, args.d_model), trainable=True )
+        self.decoder = Transformer(args.mask, args.t_layers)
+        self.fc = tf.keras.Sequential([ Dense(args.d_model, 1) ])
+        self.to_n = Dense(code.n + code.m, code.n) 
+        self.time_embed = Embedding(args.beta_steps, args.d_model)
        
-   def call(self, r_t):
+    def call(self, r_t):
        for i in range(self.pcm.shape[0]):
            r_t, z_hat, t = self.rev_diff_call(r_t)
         
            if (r_t @ self.pcm)==0:
                return r_t, z_hat, t, i            
        return r_t, z_hat, t, i
+    
+    # Refines recieved codeword r at time t
+    def rev_diff_call(self, r_t): 
+        t = ( self.pcm @ to_bin(r_t) ).sum() # ix for the 'time step' t in the diffusion # 'time step' t is really a error estimate of the syndrome ###
+        z_hat = self.rev_tran_call(r_t, t)
+        
+        factor = ( self.betas_bar[t]*self.beta[t] / (self.betas_bar[t] + self.beta[t]) ) # theoretical step size
+        err_hat = r_t - tf.sign(z_hat*rt)
+        l = self.line_search() if self.ls_active else 1.
+    
+        r_t1 = r_t - (l * factor * err_hat) # refined estimate of the codeword for the diffusion step
+        # r_t1[t==0] = r_t[t==0] # if cw has 0 synd. keep as is
+        return r_t1, z_hat, t # r at time t-1
+
+    # Extracts noise estimate z_hat from r
+    def rev_tran_call(self, r_t, t):
+        syndrome = self.pcm @ to_bin(r_t) #(m,1) check nodes
+        magnitude = tf.abs(r_t) #(n,1) variable nodes
+
+        nodes = tf.concat([magnitude, syndrome]) # data for vertices, shape (n+m,1)
+        
+        emb = self.src_emb * nodes
+        time_emb = self.time_embed(t) # t is really error estimate of syndrome ###
+        
+        emb_t = time_emb * emb
+        emb_t = self.decoder(emb_t, self.mask, time_emb)
+        z_hat = self.fc(emb_t)
+        return z_hat
 
 
 # Construct generator (encoder using forward diffusion to simulate channel)
@@ -122,6 +133,9 @@ class Generator(Layer):
     def call(self, c):
         emb = 
         return z
+
+    def fwd_dif_call(self,):
+        pass
 
         
 
