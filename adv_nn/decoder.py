95d76d46b4e0c6db982caa340bbbb40fb96f7a1f
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-08 22:18:00 -0400

    Update decoder.py

--- adv_nn/decoder.py
+++ adv_nn/decoder.py
@@ -1,9 +1,22 @@
+import copy
+c = copy.deepcopy()
+
+
 class Decoder():
-    def init(self, N):
-      self.N = N
-      self.transformer = # mask
+    def init(self, mask, N):
+      self.transformer_layers = [ Transformer(MHAttention, FeedForward, PreNorm, mask) for _ in range(N) ]
 
     def call(self, x):
-        for _ in range(self.N):
-            x = self.transformer(x)
+        for transformer in self.transformer_layers:
+            x = transformer(x, mask)
         return x
+
+
+class Transformer():
+    def init(self, attn, ff, norm, mask):
+        self.attn, self.ff = attn, ff, 
+        self.norm1, self.norm2 = c(norm), c(norm)
+
+    def call(self, x):
+        out = self.norm1(self.attn(x))
+        return self.norm2(self.ff(out))
