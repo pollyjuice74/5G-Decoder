87047f3b293bc85bc597d2a04559d29d7076edfb
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-09 21:50:08 -0400

    Update args.py

--- adv_nn/args.py
+++ adv_nn/args.py
@@ -4,7 +4,8 @@ from DDECC.src.codes import Get_Generator_and_Parity
 
 class Args():
     def __init__(self, model_type, code_type='LDPC', n=121, k=80,  ls_active=True, sigma=0.1,
-                       beta_steps=10, t_layers=1, d_model=8, lr=5e-4, epochs=1000):
+                       beta_steps=10, t_layers=1, d_model=8, lr=5e-4, batch_size=128,
+                       traindata_len=500, testdata_len=250, epochs=1000):
         assert model_type in ['gen', 'dis'], "Type must be: 'gen', Generator or 'dis', Discriminator."
         assert code_type in ['POLAR', 'BCH', 'CCSDS', 'LDPC', 'MACKAY'], "Invalid linear code type."
         
@@ -21,7 +22,10 @@ class Args():
         self.t_layers = t_layers # transformer layers
         self.d_model = d_model
         self.N_steps = self.code.H.shape[0]+5 # Number of diffusion steps
-        self.epochs = epochs
+        self.epochs = epochs #
+        self.batch_size = batch_size # chunks 
+        self.traindata_len = traindata_len
+        self.testdata_len = testdata_len
 
         self.lr = lr
     
