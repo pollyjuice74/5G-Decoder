51274df968e6f12b8951cbb817723fa1719b1dc9
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-11 10:22:14 -0400

    Update model_functs.py

--- adv_nn/model_functs.py
+++ adv_nn/model_functs.py
@@ -1,10 +1,23 @@
 from metrics import compute_bler as bler
 
-def train(model, train_loader, optimizer, epoch, LR):
-    pass
+def train_dis(model, train_loader, optimizer, epoch, LR):
+    t = time.time()
+    
+    for batch_idx, (m, x, z, y, magnitude, syndrome) in enumerate(train_loader):
+        with tf.GradientTape() as tape:
+            loss = model.train(x)
+            
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+        if (batch_idx + 1) % 10 == 0 or batch_idx == len(train_loader) - 1:
+            print(f'Training epoch {epoch}, Batch {batch_idx + 1}/{len(train_loader)}: LR={LR:.2e}, Loss={cum_loss / cum_samples:.5e}')
+            
+    print(f'Epoch {epoch} Train Time {time.time() - t}s\n')
+            
 
 
-def test(model, test_loader_list, EbNo_range_test, min_FER=100, max_cum_count=1e7, min_cum_count=1e5):
+def test_dis(model, test_loader_list, EbNo_range_test, min_FER=100, max_cum_count=1e7, min_cum_count=1e5):
     printed = False
     ber_list, bler_list = [], []
 
@@ -25,8 +38,8 @@ def test(model, test_loader_list, EbNo_range_test, min_FER=100, max_cum_count=1e
     return { "ber": ber_list, "bler": bler_list }
 
 
-def test_models():
-    data = { "LTDM": dict() }
+def test_models(): 
+    data = { "LTDM": dict() } # only decoders
 
     for ix, tst_dataset in enumerate(test_ebnos_datasets):
         print(f"\nTesting on {list(dataset_types.keys())[ix].upper()}")
