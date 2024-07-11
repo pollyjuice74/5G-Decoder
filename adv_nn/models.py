602ef0d85ed54630a52990c52a208429746c10e9
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-10 21:59:42 -0400

    Update models.py

--- adv_nn/models.py
+++ adv_nn/models.py
@@ -16,31 +16,10 @@ from DDECC.src.codes import Get_Generator_and_Parity
     # Could store different weights for generator and discriminator, 
     # make a super noise generator that will always attack really good at the discriminator
 
-
 # Base model for the Generator and Discriminator models, 
     # contains diffusion computations, line search and creates mask of pcm.
 
-class TransformerDiffusion():
-    def __init__(self):
-        pass
-
-    # optimal lambda l for theoretical and for error prediction
-    def line_search(self, factor, err_hat):
-        l_values = tf.linespace(1., 20., 20).reshape(1,1,20)
-        syndromes = to_bin(r_t - l_values*(factor*err_hat)) @ self.pcm
-        ix = syndromes.argmin() # get the index of the min syndrome
-        return l_values[ix]
-
-
-# Construct discriminator (decoder using reverse diffusion)
-    # Will have to come up with ways to try to decode the noised codeword against specific noise
-    # that will be trying to fool it. 
-    
-    # For optimization:
-        # use Linformer having a O(n) on top of already improved complexity using the pcm mask
-        # use split diffusion to improve accuracy and efficiency by guiding model rather than EMA
-
-class Discriminator(TransformerDiffusion):
+class TransformerDiffusion(Layer):
     def __init__(self, args):
         self.betas = tf.linspace(1e-3, 1e-2, args.beta_steps)*0 + args.sigma 
         self.betas_bar = tf.math.cumsum(self.betas, 0)
@@ -52,6 +31,44 @@ class Discriminator(TransformerDiffusion):
         self.fc = tf.keras.Sequential([ Dense(1) ])
         self.to_n = Dense(code.n + code.m) 
         self.time_embed = Embedding(args.beta_steps, args.d_model)
+
+    # optimal lambda l for theoretical and for error prediction
+    def line_search(self, sigma, err_hat, model_type='dis'):
+        assert model_type in ['dis','gen'], "Invalid model type."
+        l_values = tf.linespace(1., 20., 20).reshape(1,1,20)
+        syndromes = to_bin(r_t - l_values*(sigma*err_hat)) @ self.pcm
+        
+        ix = syndromes.argmin() if model_type=='dis' else 
+             syndromes.argmax() if model_type=='gen' # get the index of the min syndrome
+        return l_values[ix]
+
+    # Extracts noise estimate z_hat from r
+    def tran_call(self, r_t, t):
+        syndrome = self.pcm @ to_bin(r_t) #(m,1) check nodes
+        magnitude = tf.abs(r_t) #(n,1) variable nodes
+
+        nodes = tf.concat([magnitude, syndrome]) # data for vertices, shape (n+m,1)
+        
+        emb = self.src_emb * nodes
+        time_emb = self.time_embed(t) # t is really error estimate of syndrome ###
+        
+        emb_t = time_emb * emb
+        emb_t = self.decoder(emb_t, self.mask, time_emb)
+        z_hat = self.fc(emb_t)
+        return z_hat 
+
+
+# Construct discriminator (decoder using reverse diffusion)
+    # Will have to come up with ways to try to decode the noised codeword against specific noise
+    # that will be trying to fool it. 
+    
+    # For optimization:
+        # use Linformer having a O(n) on top of already improved complexity using the pcm mask
+        # use split diffusion to improve accuracy and efficiency by guiding model rather than EMA
+
+class Discriminator( TransformerDiffusion ):
+    def __init__(self):
+        super().__init__()
        
     def call(self, r_t):
        for i in range(self.pcm.shape[0]):
@@ -64,31 +81,16 @@ class Discriminator(TransformerDiffusion):
     # Refines recieved codeword r at time t
     def rev_diff_call(self, r_t): 
         t = ( self.pcm @ to_bin(r_t) ).sum() # ix for the 'time step' t in the diffusion # 'time step' t is really a error estimate of the syndrome ###
-        z_hat = self.rev_tran_call(r_t, t)
+        z_hat = self.tran_call(r_t, t)
         
         sigma = ( self.betas_bar[t]*self.beta[t] / (self.betas_bar[t] + self.beta[t]) ) # theoretical step size
-        err_hat = r_t - tf.sign(z_hat*rt)
-        l = self.line_search() if self.ls_active else 1.
+        err_hat = r_t - tf.sign(z_hat*r_t)
+        l = self.line_search(sigma, err_hat) if self.ls_active else 1.
     
         r_t1 = r_t - (l * sigma * err_hat) # refined estimate of the codeword for the diffusion step
         # r_t1[t==0] = r_t[t==0] # if cw has 0 synd. keep as is
         return r_t1, z_hat, t # r at time t-1
-
-    # Extracts noise estimate z_hat from r
-    def rev_tran_call(self, r_t, t):
-        syndrome = self.pcm @ to_bin(r_t) #(m,1) check nodes
-        magnitude = tf.abs(r_t) #(n,1) variable nodes
-
-        nodes = tf.concat([magnitude, syndrome]) # data for vertices, shape (n+m,1)
         
-        emb = self.src_emb * nodes
-        time_emb = self.time_embed(t) # t is really error estimate of syndrome ###
-        
-        emb_t = time_emb * emb
-        emb_t = self.decoder(emb_t, self.mask, time_emb)
-        z_hat = self.fc(emb_t)
-        return z_hat
-
 
 # Construct generator (encoder using forward diffusion to simulate channel)
     # By simulating channel it will try to come up with ways to fool discriminator/decoder
@@ -97,28 +99,26 @@ class Discriminator(TransformerDiffusion):
     # Could make a general generator for GNN, ECCT, DDECCT, and AECC(Adversarial Error Correcting Code) 
     # to compare how different models learn or have weaknesses in decoding. 
 
-class Generator(Layer):
+class Generator( TransformerDiffusion ):
     def __init__(self):
-        pass
+        super().__init__()
 
-    def call(self, c):
-        pass
-        # emb = 
-        # return z
+    def call(self, c_t):
+       for i in range(self.pcm.shape[0]):
+           c_t, z_G = self.fwd_diff_call(c_t, z_G)          
+       return z_G
 
-    def fwd_diff_call(self, x_0):
-        x_t = x_0
-        z = self.fwd_tran_call()
+    def fwd_diff_call(self, c_t, z_G):
+        t = ( self.pcm @ to_bin(r_t) ).sum()
+        z_G = self.tran_call(c_t, t)
         
-        err = 
-        sigma = 
-        l = 
+        sigma = ( self.betas_bar[t]*self.beta[t] / (self.betas_bar[t] + self.beta[t]) )
+        noise = c_t - tf.sign(c_t*z_G)
+        l = self.line_search(sigma, noise, model_type='gen') if self.ls_active else 1.
         
-        x_t1 =  x_t + (l * sigma * err)
-        return x_t1
+        c_t =  c_t + (l * sigma * noise)
+        return c_t, z_G
         
-    def fwd_tran_call(self,):
-        pass
 
         
 
