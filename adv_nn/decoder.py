8a9276c491d15ff7b50a057824ad14c5eabc1d29
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-15 11:22:34 -0400

    Update decoder.py

--- adv_nn/decoder.py
+++ adv_nn/decoder.py
@@ -1,13 +1,14 @@
-import tensorflow as tf
+from tensorflow.keras.layers import Layer
 import copy
 c = lambda x: copy.deepcopy(x)
 
 from attention import *
 
 
-class Transformer(tf.keras.layers.Layer):
+class Transformer(Layer):
     def __init__(self, mask, N):
-      self.transformer_layers = [ TransformerLayer(MHAttention, FeedForward, PreNorm, mask) for _ in range(N) ]
+        super().__init__()
+        self.transformer_layers = [ TransformerLayer(MHAttention, FeedForward, PreNorm, mask) for _ in range(N) ]
 
     def call(self, x):
         for transformer in self.transformer_layers:
@@ -15,12 +16,13 @@ class Transformer(tf.keras.layers.Layer):
         return x
 
 
-class TransformerLayer(tf.keras.layers.Layer):
+class TransformerLayer(Layer):
     def __init__(self, attn, ff, norm, mask):
+        super().__init__()
         self.attn, self.ff = attn, ff
         self.norm1, self.norm2 = c(norm), c(norm)
         self.mask = mask
 
     def call(self, x):
         out = self.norm1( self.attn(x, self.mask) )
-        return self.norm2(self.ff(out))
+        return self.norm2( self.ff(out) )
