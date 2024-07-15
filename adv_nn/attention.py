0e6c2483a26874a04633f0e567258ca7b27831c3
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-15 16:49:52 -0400

    Update attention.py

--- adv_nn/attention.py
+++ adv_nn/attention.py
@@ -6,6 +6,7 @@ from tensorflow.keras.activations import gelu
 
 class FeedForward(Layer):
     def __init__(self, dim, mult=4, activation=None, dropout=0.01):
+        super().__init__()
         self.w1 = Dense(dim * mult)
         self.w2 = Dense(dim)
         self.dropout = Dropout(dropout) 
@@ -18,6 +19,7 @@ class FeedForward(Layer):
 
 class PreNorm(Layer):
     def __init__(self, dropout=0.01):
+        super().__init__()
         self.norm = LayerNormalization()
         self.dropout = Dropout(dropout)
         
@@ -26,30 +28,36 @@ class PreNorm(Layer):
 
 
 class MHAttention(Layer):
-    def __init__(self, dims, heads, linear=True, k=256, dropout=0.01):
+    def __init__(self, dims, heads, linear=True, k_proj=256, dropout=0.01):
         super().__init__()
         assert (dims % heads) == 0, 'dimension must be divisible by the number of heads'
         self.linear = linear
         self.dims = dims
         self.heads = heads
         self.dim_head = dims // heads
-        self.k = k
 
         if linear:
-            self.proj_k = self.add_weight("proj_k", shape=[dims, k], initializer=GlorotUniform())
-            self.proj_v = self.add_weight("proj_v", shape=[dims, k], initializer=GlorotUniform())
-        
-        self.to_q, self.to_k, self.to_v = [ Dense(self.dim_head * heads, use_bias=False) for _ in range(3) ]
+            self.k_proj = k_proj
+            self.proj_k = None
+            self.proj_v = None              
+
+        self.to_q, self.to_k, self.to_v = [ Dense(self.dims, use_bias=False) for _ in range(3) ]
         self.to_out = Dense(dims)
         self.dropout = Dropout(dropout) # to d-dimentional embeddings
             
     def call(self, x, mask=None):
         out_att = self.lin_attention(x, mask) if self.linear else self.attention(x, mask)
-        return out_attn
+        return out_att
 
     def lin_attention(self, x, mask): # O(n)
         b,n,_ = tf.shape(x)
         query, key, val = self.to_q(x), self.to_k(x), self.to_v(x)
+
+        # Creates shape (n,k_proj) proj matrices for key and val 
+        if self.proj_k is None or self.proj_v is None: 
+            # TODO: Could make projs dense layers #
+            self.proj_k = self.add_weight("proj_k", shape=[n, self.k_proj], initializer=GlorotUniform())
+            self.proj_v = self.add_weight("proj_v", shape=[n, self.k_proj], initializer=GlorotUniform())
 
         # Project key and val into k-dimentional space
         key = tf.einsum('bnd,nk->bkd', key, self.proj_k)
@@ -57,14 +65,19 @@ class MHAttention(Layer):
 
         # Reshape splitting for heads
         query = tf.reshape(query, (b, n, self.heads, self.dim_head))
-        key = tf.reshape(key, (b, self.k, self.heads, self.dim_head))
-        val = tf.reshape(val, (b, self.k, self.heads, self.dim_head))
-        query, key, value = [ tf.transpose(x, [0, 2, 1, 3]) for x in [query, key, value] ]
+        key = tf.reshape(key, (b, self.k_proj, self.heads, self.dim_head))
+        val = tf.reshape(val, (b, self.k_proj, self.heads, self.dim_head))
+        query, key, val = [ tf.transpose(x, [0, 2, 1, 3]) for x in [query, key, val] ]
 
-        # Main attn logic: sftmx( q@k / d**-0.5 ) @ v 
-        scores = tf.einsum('bhnd,bhkd->bhnk', query, key) / (tf.sqrt(self.dim_head))
+        # Low-rank mask (n,k_proj)
+        mask = tf.expand_dims(mask, axis=-1)
+        mask = tf.image.resize(mask, [n, self.k_proj], method='nearest')
+        mask = tf.reshape(mask, (1, 1, n, self.k_proj))
+
+        # Main attn logic: sftmx( q@k / d**0.5 ) @ v 
+        scores = tf.einsum('bhnd,bhkd->bhnk', query, key) / (tf.sqrt( tf.cast(self.dim_head, dtype=tf.float32) ))
         scores += (mask * -1e9) if mask is not None else 0.
-        attn = tf.nn.softmax(scores, axis=0) #-1
+        attn = tf.nn.softmax(scores, axis=-1) # (b,h,n,k_proj)
         attn = self.dropout(attn)
         out = tf.einsum('bhnk,bhkd->bhnd', attn, val)
 
@@ -80,7 +93,7 @@ class MHAttention(Layer):
         query, key, val = [ tf.transpose(x, [0, 2, 1, 3]) for x in [query, key, val] ]
 
         scores = tf.einsum('bhnd,bhnd->bhnn', query, key) / (tf.sqrt(self.dim_head))
-        scores += (mask * -1e9) if mask is not None else 0.
+        scores += (mask * -1e9) if mask is not None else 0. # apply mask non-edge connections
         attn = tf.nn.softmax(scores, axis=-1) #-1 
         attn = self.dropout(attn)
         out = tf.einsum('bhnn,bhnm->bhnd', attn, v)
