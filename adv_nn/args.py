17b48482de0f6442cbd35a39e574be99b19d296d
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-18 11:45:54 -0400

    Update args.py

--- adv_nn/args.py
+++ adv_nn/args.py
@@ -6,7 +6,8 @@ from .decoder5G import LDPC5GDecoder
 
 
 class Args():
-    def __init__(self, model_type, code_type='LDPC', n_look_up=121, k_look_up=80, n_rings=2, ls_active=True, sigma=0.1,
+    def __init__(self, model_type, code_type='LDPC', n_look_up=121, k_look_up=80, n=400, k=200,
+                       n_rings=2, ls_active=True, sigma=0.1,
                        t_layers=1, d_model=128, heads=8, lr=5e-4, batch_size=128, 
                        traindata_len=500, testdata_len=250, epochs=1000):
         assert model_type in ['gen', 'dis'], "Type must be: 'gen', Generator or 'dis', Discriminator."
@@ -31,8 +32,13 @@ class Args():
 
         # Ensure that code, m, and n are set properly
         self.code = self.get_code(n_look_up, k_look_up) # n,k look up values in Get_Generator_and_Parity
-        self.n, self.m, self.k = self.code.n, self.code.m, self.code.k
-        self.n_steps = self.code.m + 5  # Number of diffusion steps
+        
+        if code_type not in ['LDPC5G', 'POLAR5G']:
+            self.n, self.m, self.k = self.code.n, self.code.m, self.code.k
+        else: 
+            self.n, self.m, self.k = n, n-k, k
+
+        self.n_steps = self.m + 5  # Number of diffusion steps
 
     def get_code(self, n_look_up, k_look_up):
         code = type('Code', (), {})() # class Code, no base class, no attributes/methods, () instantiate object
@@ -40,6 +46,7 @@ class Args():
         code.code_type = self.code_type
     
         if self.code_type=='LDPC5G':
+            pass
             # encoder = LDPC5GEncoder(k,n)
             # decoder = LDPC5GDecoder(encoder)
             # code.enc = encoder
@@ -54,7 +61,8 @@ class Args():
             G, H = Get_Generator_and_Parity(code)
             code.G, code.H = tf.convert_to_tensor(G), tf.convert_to_tensor(H)
         
-        code.m, code.n = code.H.shape
-        code.k = code.n - code.m
+            code.m, code.n = code.H.shape
+            code.k = code.n - code.m
+
         return code
         
