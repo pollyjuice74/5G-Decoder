6766c942c18b577267ecc76d8267960bc177f341
pollyjuice74 <104398042+pollyjuice74@users.noreply.github.com>
2024-07-24 09:42:58 -0400

    Update transformer.py

--- adv_nn/transformer.py
+++ adv_nn/transformer.py
@@ -8,7 +8,9 @@ from attention import *
 class Transformer(Layer):
     def __init__(self, d_model, heads, mask, N):
         super().__init__()
-        self.transformer_layers = [ TransformerLayer( MHAttention(d_model, heads), FeedForward(d_model), PreNorm() ) for _ in range(N) ]
+        self.transformer_layers = [ TransformerLayer( MHAttention(d_model, heads, mask_length=mask.shape[0]),
+                                                      FeedForward(d_model),
+                                                      PreNorm() ) for _ in range(N) ]
         self.mask = mask
 
     def call(self, x):
